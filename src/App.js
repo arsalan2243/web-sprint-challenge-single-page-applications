3d201da8c5198fc1b773ee5c31bf7f928cc7dbe7
arsalan rahimi <years1373@yahoo.com>
2021-10-15 11:22:31 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Help from "./components/Help";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <h1>Lambda Eats</h1>
+        <div className="Links">
+          <Link to="/">Home</Link>
+          <Link to="/help">Help</Link>
+        </div>
+      </nav>
+      <Switch>
+        <Route path="/help" component={Help} />
+        <Route path="/" component={Home} />
+      </Switch>
     </>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Your Favorite Food!</h1>
+      <button>Pizza</button>
+      <img className="home-image" src="../Pizza.jpg" alt="" />
+    </div>
+  );
+};
+
+export default Home;
